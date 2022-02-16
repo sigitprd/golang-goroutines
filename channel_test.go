7eeb9cb96a2de29c6cf546717942b0e6ac58a1e0
Sigit Priadi <sigitpriadi23@gmail.com>
2022-02-16 21:42:26 +0700

    test create channel

---
+++ channel_test.go
@@ -0,0 +1,25 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Sigit Priadi"
+		fmt.Println("Selesai mengirim data ke Channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+
+	// fmt.Println(<-channel)
+}
