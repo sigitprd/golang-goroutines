482ac0d0ddc7f1e103b763a9cca41d25766700ad
Sigit Priadi <sigitpriadi23@gmail.com>
2022-02-17 20:42:56 +0700

    test gomaxprocs

---
+++ gomaxprocs_test.go
@@ -0,0 +1,31 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGetGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCPU := runtime.NumCPU()
+	fmt.Println("Total CPU:", totalCPU)
+
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread:", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine:", totalGoroutine)
+
+	group.Wait()
+}
