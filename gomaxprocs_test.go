f879bc372cc34bbfaba66b90f978ba53df4b8e22
Sigit Priadi <sigitpriadi23@gmail.com>
2022-02-17 20:45:47 +0700

    test change thread

--- gomaxprocs_test.go
+++ gomaxprocs_test.go
@@ -29,3 +29,26 @@ func TestGetGomaxprocs(t *testing.T) {
 
 	group.Wait()
 }
+
+func TestChangeThreadNumber(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCPU := runtime.NumCPU()
+	fmt.Println("Total CPU:", totalCPU)
+
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread:", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine:", totalGoroutine)
+
+	group.Wait()
+}
